77878179b5bd40c413b9748c93d881cbb562dab2
Hippopoi1234 <89053907+Hippopoi1234@users.noreply.github.com>
2021-08-17 16:42:21 +0500

    Add files via upload

---
+++ TODO.py
@@ -0,0 +1,13 @@
+a = int(input("Сколько дел тебе надо сделать? "))
+b = []
+for i in range(a):
+    b.append(input())
+for i in enumerate(b, 1):
+    print(str(i[0]) + ")", i[1])
+while not len(b) == 0:
+    d = input("Хочешь ли ты удалить задачу? ")
+    if d == "да":
+        c = int(input("Номер задачи, которую надо удалить ")) - 1
+        b.remove(b[c])
+        for i in enumerate(b, 1):
+            print(str(i[0]) + ")", i[1])
