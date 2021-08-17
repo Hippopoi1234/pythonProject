8cd06c0a5b50e10b8eda5115e7059ea9116cd4db
Hippopoi1234 <89053907+Hippopoi1234@users.noreply.github.com>
2021-08-17 07:23:37 +0500

    Update nomer1.py

--- codeforces/nomer1.py
+++ codeforces/nomer1.py
@@ -1,7 +1,4 @@
-def greet(a, *args):
-    if len(args) == 0:
-        print("Hello,", a)
-    else:
-        print("Hello", a, end=" ")
-        print(*args, sep="and", end="")
-        print("!")
+def greet(*args):
+    print("Hello", end=" ")
+    print(*args, sep="and", end="")
+    print("!")
