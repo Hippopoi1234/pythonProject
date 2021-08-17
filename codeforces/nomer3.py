14e6091908e6b02496bdd934b1139746465ac8e3
Hippopoi1234 <89053907+Hippopoi1234@users.noreply.github.com>
2021-08-17 07:30:43 +0500

    Delete nomer3.py

--- codeforces/nomer3.py
+++
@@ -1,9 +0,0 @@
-# for i in range(10, 100):
-#     if i % 17 == 0:
-#         print(17, i)
-#     elif i % 23 == 0:
-#         print(23, i)
-print(92346)
-print(923468517)
-print(2018 % 5)  # 3
-print((2018 - 9) % 5)
