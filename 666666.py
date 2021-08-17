37bf808be6bec07c17dac16a7335ffaaece235f5
Hippopoi1234 <a_grigoriev@outlook.com>
2021-08-17 06:51:34 +0500

    IDK

---
+++ 666666.py
@@ -0,0 +1,48 @@
+# k = 0
+# for i in range(1000, 10000):
+#     i = str(i)
+#     if "9" in i and not ("3" in i):
+#         # print(i)
+#         k += 1
+# print("grustnie" ,k)
+# A=[]
+# for i in range(4,21):
+#     for x in range(2,4):
+#         for y in range(2,4):
+#             if x*y==i or x*x==i or y*y==i or x*x*y==i or y*y*x==i:
+#                 A.append(i)
+# print(A)
+# k = 0
+# for i in range(1000, 10000):
+#     i = str(i)
+#     if "3" in i and not ("9" in i):
+#         # print(i)
+#         k += 1
+# print("veselie" ,k)
+
+# k=0
+# for i1 in [8]:
+#     for i2 in [9]:
+#         for i3 in [4]:
+#             for i4 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#                 for i5 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#                     for i6 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#                         i = str(i1)+str(i2)+str(i3)+str(i4)+str(i5)+str(i6)
+#                         if int(i)%4==0 and int(i)%5==0 and int(i)%9==0:
+#                             print(i, sep="")
+#                             k+=1
+# print(k)
+# a =[]
+# for i1 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#     for i2 in [2]:
+#         for i3 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#             for i4 in [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+#                         i = i1+i2+i3+i4
+#                         if i == 18 and i1!=i2 and i1!=i3 and i1!=i4 and i2!=i3 and i3!=i4 and i2!=i4:
+#                             a.append(int(str(i1)+str(i2)+str(i3)+str(i4)))
+# a.sort()
+# print(a)
+# r = set(a)
+# print(len(r), r)
+print((239*10000000*3)+(239*100000)+(239*1000-239*10*2+239*7))
+print(10000000*3+100000+(1000-20+7)+ 6013)
