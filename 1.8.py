a28fcccc62ff9f19b98ec278e8379a55eabd9809
Hippopoi1234 <a_grigoriev@outlook.com>
2021-08-17 07:50:06 +0500

    It is a problem.

---
+++ 1.8.py
@@ -0,0 +1,5 @@
+student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013'] 
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy'] 
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+result = [{student_ids[i]:{student_names[i]:student_grades[i]}} for i in range(len(student_grades))]
+print(result)
